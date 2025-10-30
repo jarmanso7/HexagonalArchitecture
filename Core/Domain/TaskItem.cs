0c3d9b6b8599aeba55775724661a9edb52c715ff
jarmanso7 <jordi.armangue@hotmail.com>
2025-10-30 22:03:36 +0100

    Added domain and ports

---
+++ Core/Domain/TaskItem.cs
@@ -0,0 +1,21 @@
+﻿namespace TaskManagement.Core.Domain
+{
+    public class TaskItem
+    {
+        public Guid Id { get; }
+        public string Title { get; private set; }
+        public bool IsCompleted { get; private set; }
+
+        public TaskItem(string title)
+        {
+            Id = Guid.NewGuid();
+            Title = title;
+            IsCompleted = true;
+        }
+
+        public void Complete()
+        {
+            IsCompleted = true;
+        }
+    }
+}
---
+++ Core/Ports/Driven/ITaskRepository.cs
@@ -0,0 +1,11 @@
+﻿using TaskManagement.Core.Domain;
+
+namespace TaskManagement.Core.Ports.Driven
+{
+    public interface ITaskRepository
+    {
+        void Save(TaskItem task);
+        void Update(TaskItem task);
+        TaskItem? GetById(Guid id);
+    }
+}

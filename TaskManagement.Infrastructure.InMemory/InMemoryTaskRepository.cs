dad5ccde0d8eb7279f535f58745f3f2d27e39de6
jarmanso7 <jordi.armangue@hotmail.com>
2025-10-30 22:48:16 +0100

    Implemented Driven port, a.k.a. added Driven Adapter

---
+++ TaskManagement.Infrastructure.InMemory/InMemoryTaskRepository.cs
@@ -0,0 +1,30 @@
+﻿using TaskManagement.Core.Domain;
+using TaskManagement.Core.Ports.Driven;
+
+namespace TaskManagement.Infrastructure.InMemory
+{
+    public class InMemoryTaskRepository : ITaskRepository
+    {
+        private Dictionary<Guid, TaskItem> tasks = new();
+
+        public void Save(TaskItem task)
+        {
+            if (tasks.ContainsKey(task.Id))
+                throw new Exception("A task with the same Id already exists");
+
+            tasks.Add(task.Id, task);
+        }
+
+        public void Update(TaskItem task)
+        {
+            if (! tasks.ContainsKey(task.Id))
+                throw new Exception($"Task with id {task.Id} not found.");
+
+            tasks[task.Id] = task;
+        }
+        public TaskItem? GetById(Guid id)
+        {
+            return tasks.TryGetValue(id, out TaskItem value) ? value : null;
+        }
+    }
+}

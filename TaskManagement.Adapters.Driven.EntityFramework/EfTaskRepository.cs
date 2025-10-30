cac09cd99901d282bf719e4d594bd428e337d642
jarmanso7 <jordi.armangue@hotmail.com>
2025-10-30 23:53:10 +0100

    Added Driven Adapter (real SQLite InMemory DB via EF) ´

---
+++ TaskManagement.Adapters.Driven.EntityFramework/EfTaskRepository.cs
@@ -0,0 +1,27 @@
+﻿using TaskManagement.Core.Domain;
+using TaskManagement.Core.Ports.Driven;
+
+namespace TaskManagement.Adapters.Driven.EntityFramework
+{
+    public class EfTaskRepository : ITaskRepository
+    {
+        private readonly TaskDbContext _context;
+
+        public EfTaskRepository(TaskDbContext context)
+        {
+            _context = context;
+        }
+
+        public TaskItem? GetById(Guid id) => _context.Tasks.Find(id);
+        public void Save(TaskItem task)
+        {
+            _context.Tasks.Add(task);
+            _context.SaveChanges();
+        }
+        public void Update(TaskItem task)
+        {
+            _context.Tasks.Update(task);
+            _context.SaveChanges();
+        }
+    }
+}
---
+++ TaskManagement.Adapters.Driven.EntityFramework/TaskDbContext.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore;
+using TaskManagement.Core.Domain;
+
+namespace TaskManagement.Adapters.Driven.EntityFramework
+{
+    public class TaskDbContext : DbContext
+    {
+        public TaskDbContext(DbContextOptions<TaskDbContext> options) : base(options)
+        {
+
+        }
+
+        public DbSet<TaskItem> Tasks { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            var taskItemBuilder = modelBuilder.Entity<TaskItem>();
+
+            taskItemBuilder.HasKey(t => t.Id);
+
+            taskItemBuilder.Property(t => t.Title)
+                .IsRequired()
+                .HasMaxLength(30);
+
+            taskItemBuilder.Property(t => t.IsCompleted);
+        }
+    }
+}
